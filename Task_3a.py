aefce0c164983ce72309c62739551727c44714b2
GandElf <momarmshami@gmail.com>
2023-12-12 00:02:31 +0100

    Fixed cam viewing direction

--- Task_3a.py
+++ Task_3a.py
@@ -1,5 +1,6 @@
 import bpy
 import math
+from mathutils import Vector
 
 # File path
 obj_file_path = r'D:\FAU\Studies\4- WS 2023\Computational Visual Perception\Project\Part 2\Dataset\colored_cloths\1.obj'
@@ -23,10 +24,13 @@ bpy.ops.import_scene.obj(filepath=obj_file_path)
 print(f"Imported {obj_file_path} successfully.")
 
 # Add a camera
-location = (2, 2, 2)
-rotation = (math.radians(50), math.radians(0), math.radians(135))
-bpy.ops.object.camera_add(location=location, rotation = rotation)
+cam_location = (2, 2, 2)
+bpy.ops.object.camera_add(location=cam_location)
 camera_object = bpy.context.object
+target_point = Vector((0, 0, -0.5))
+cam_direction = camera_object.location - target_point
+rot_quat = cam_direction.to_track_quat('Z', 'Y')
+camera_object.rotation_euler = rot_quat.to_euler()
 bpy.context.scene.camera = camera_object
 
 # Update the scene
