a6164929eb4c358f99ac4fcdee8a6af27b366d62
GandElf <momarmshami@gmail.com>
2023-12-12 02:00:46 +0100

    Random illumination added.

--- Task_3_Illumination.py
+++ Task_3_Illumination.py
@@ -2,14 +2,19 @@ import bpy
 import math
 from math import radians, sin, cos
 from mathutils import Vector
+import random
+
+# Reproducible randomization
+random_seed = 42
+random.seed(random_seed)
 
 # Counter
 total_images_rendered = 0
 
-for obj_index in range (1, 2):
+for obj_index in range (1, 3):
     # File path
     obj_file_path = f'D:\\FAU\\Studies\\4- WS 2023\\Computational Visual Perception\\Project\\Part 2\\Dataset\\colored_cloths\\{obj_index}.obj'
-    output_directory = f'D:\\FAU\\Studies\\4- WS 2023\\Computational Visual Perception\\Project\\Part 2\\Distance_Rendering'
+    output_directory = f'D:\\FAU\\Studies\\4- WS 2023\\Computational Visual Perception\\Project\\Part 2\\Illumination_Rendering'
 
     # Delete any existing meshes
     bpy.ops.object.select_all(action='DESELECT')
@@ -34,7 +39,12 @@ for obj_index in range (1, 2):
     camera_object = bpy.context.object
     target_point = Vector((0, 0, -0.5))
     bpy.context.scene.camera = camera_object
-
+    
+    # Add light source
+    bpy.ops.object.light_add(type='POINT', location=(0, 0, 3))
+    light_object = bpy.context.object
+    light_object.data.energy = 500
+ 
     # Update the scene
     bpy.context.view_layer.update()
 
@@ -45,22 +55,36 @@ for obj_index in range (1, 2):
     bpy.context.scene.render.image_settings.color_mode = 'BW'
     bpy.context.scene.render.image_settings.color_depth = '8'
     bpy.context.scene.render.image_settings.use_zbuffer = False
-
-    # Rotate camera around object and capture frames
+    
+    # Set background to black
+    bpy.context.scene.world.node_tree.nodes["Background"].inputs["Color"].default_value = (0, 0, 0, 1)
+     
+    # Change scene and render the images
     num_frames = 50
     rotation_radius = 4
+    
     for img_index in range(num_frames):
         angle = radians((img_index / num_frames) * 360)
+        
+        # Rotate camera around the object
         camera_object.location = (
             rotation_radius * cos(angle),
             rotation_radius * sin(angle),
             cam_location[2],
         )
+        
         # Keep the camera view at target point
         cam_direction = camera_object.location - target_point
         rot_quat = cam_direction.to_track_quat('Z', 'Y')
         camera_object.rotation_euler = rot_quat.to_euler()
-
+        
+        # Change light source location
+        light_object.location = (
+            rotation_radius * cos(radians(random.uniform(0, 360))),
+            rotation_radius * sin(radians(random.uniform(0, 360))),
+            light_object.location[2],
+        )
+        
         # Set the output path for the rendered image
         total_images_rendered += 1
         bpy.context.scene.render.filepath = f"{output_directory}\\{total_images_rendered}"
